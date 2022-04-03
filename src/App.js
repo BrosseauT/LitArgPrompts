5a237d9afc309d5aac5922b887bd7bb2919f3bc3
scotch115 <jordangamache115@gmail.com>
2022-04-03 17:02:14 -0400

    Fixed db reset

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { Switch, FormGroup, FormControlLabel } from '@mui/material';
       localprompts: [],
       activeprompt: '',
       activepromptdate: '',
-      checked: false
+      checked: false,
+      lastprompt: '',
     }
   };
   
@@ -19,22 +20,21 @@ import { Switch, FormGroup, FormControlLabel } from '@mui/material';
         firebase.database().ref("/").on("value", snapshot => {
 
       snapshot.forEach(snap => {
-        allTmp.push(`${snap.ref.key}: ${snap.val()}`);
-      
+        allTmp.push(`${snap.ref.key}: ${snap.val()}`);      
       });
       
     });
-    this.setState({ localprompts: allTmp,});
+    this.setState({ localprompts: allTmp });
     this.saveDB();
   };
 
-componentDidUpdate() {
-  if (this.state.localprompts === []) {
-    this.populateLocalDB();
-  } else {
-    this.saveDB();
+  componentDidUpdate() {
+    if (this.state.localprompts === []) {
+      this.populateLocalDB();
+    } else {
+      this.saveDB();
+    }
   }
-}
 
   saveDB() {
     const items = [];
@@ -60,56 +60,77 @@ componentDidUpdate() {
   populateLocalDB() {
     let allTmp = [];
     firebase.database().ref("/").on("value", (snapshot) => {
-        snapshot.forEach((snap) => {
-           allTmp.push(`${snap.ref.key}: ${snap.val()}`);  
-          });      
-        });
-       this.setState({ localprompts: allTmp});
+      snapshot.forEach((snap) => {
+          allTmp.push(`${snap.ref.key}: ${snap.val()}`);  
+        });      
+      });
+    this.setState({ localprompts: allTmp });
     this.saveDB();
   };
     
-  generatePrompt = () => {
-      var rand = Math.floor(Math.random() * 42);
+    generatePrompt = () => {
+      var rand = Math.floor(Math.random() * this.state.localprompts.length);
       if (this.state.checked) {
         this.deleteItem(this.state.activeprompt);
-        this.setState({
-          checked: false
-        })
-      }  
+      } else {
         this.setState({
           activeprompt: this.state.localprompts[rand]
         });
-      // console.log(this.state.localprompts[rand]);
-     };
+      }
+    };
 
      deleteItem(activePrompt) {
-       console.log(JSON.parse(localStorage.getItem('prompts')))
-      var localDB = [...JSON.parse(localStorage.getItem('prompts'))];
-      for (var i = 0; i < localDB.length; i++) {
-        if (localDB[i] === activePrompt) {
-          localDB.splice(i, 1);
-        }
-      }
-      localStorage.setItem('prompts', JSON.stringify(localDB));
-     };
+      // Make a temporary copy of our state DB of prompts
+      var localDB = [...this.state.localprompts];
+      // Check if the current prompt exists in localStorage
+      if (localStorage.prompts.includes(activePrompt)) {
+        // If it does, save the item's DB location (index)
+        var deleteIndex = localDB.indexOf(activePrompt);
+      
+        // Remove the item in localDB
+        localDB.splice(deleteIndex, 1);
 
-  render() {
-    // console.log(this.state.localprompts);
-    return (
-    <div className="App">
-     <h1>Literary Argument Prompt Generator</h1>
-    { (this.state.activeprompt !== '')
-    ?  <div className="prompt">
-    {this.state.activeprompt} 
-      </div>
-    : <div className="placeholdertext">
-      <p>
+        var rand = 0;
+        if (localDB.length > 1) {
+          rand = Math.floor(Math.random() * localDB.length);
+        }
+        // Set localStorage to the updated DB
+        localStorage.setItem('prompts', JSON.stringify(localDB));
+        // Overwrite the component state to use the updated DB
+        this.setState({ localprompts: localDB, activeprompt: localDB[rand] });
+      }
+    }
+
+    renderContent() {
+      if (this.state.localprompts === [] || ( this.state.activeprompt === undefined && this.state.localprompts.length === 0 )) {
+        return (<div className="prompt"> Hi! So ya broke it huh? No worries, let's go ahead and reset the database and start fresh :).</div>);
+      } 
+      if (this.state.activeprompt !== '' && this.state.localprompts !== [] && this.state.activeprompt !== undefined) {
+        return (<div className="prompt">
+          {this.state.activeprompt} 
+        </div>);
+      } else {
+        return (<div className="placeholdertext">
+        <span>
           <h3>This page can be used to generate a random Literary Argument prompt from previous College Board AP Literature exams. </h3> <br />
           A full list of prompts can be found <a href="https://docs.google.com/document/d/1H1Q8N5WpAG7sgtCROOYMcOrmRFZR4EkxoDT_MRQLPWU/edit?usp=sharing">here</a>. Alternatively, they can be found on the College Board Website <a href='https://apcentral.collegeboard.org/courses/ap-english-literature-and-composition/exam/past-exam-questions?course=ap-english-literature-and-composition'>here</a>. <br />
           This website has no official affiliation with College Board. They own all the rights to all of this, I just made it for fun. 
-       </p>
-      </div>
+        </span>
+        </div>);
+      }
     }
+
+    resetDB() {
+      this.setState({ checked: false, activeprompt: '' }, () => {
+        this.populateLocalDB();
+      });
+    }
+
+  render() {
+    return (
+    <div className="App">
+     <h1>Literary Argument Prompt Generator</h1>
+    {this.renderContent()}
     <div className="generatewrapper">
       <button type="submit" style={{ marginRight: '35px', height: '32px'}} onClick={() => this.generatePrompt()}> Generate a Random Prompt </button> <br /> <br />
       { (this.state.activeprompt !== '') 
@@ -131,8 +152,35 @@ componentDidUpdate() {
       : null}
     </div>
     { (this.state.activeprompt !== '') 
-      ? <button onClick={() => {this.setState({  activeprompt: '' })}}> Return Home </button>
-      : null}
+        ? <div
+            style={{
+              marginTop: '16px',
+              height: '32px',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center'
+            }} 
+          > 
+            <button onClick={() => {this.setState({  activeprompt: '' })}}> Return Home </button>
+          </div>
+        : null
+      }
+    <div
+      style={{
+        marginTop: '16px',
+        height: '32px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+      }} 
+    >
+      <button
+        type="submit"
+        onClick={() => {
+          this.resetDB();
+        }
+      }> Reset Database </button>
+    </div>
    </div>
   )
 }
